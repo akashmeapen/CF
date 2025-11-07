767396d6e789510f23c9704583b33fc1f96218bb
Akash Modayil Eapen <akash.modayil@gmail.com>
2025-11-08 02:44:35 +0530

    croma added

---
+++ CF/croma.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+        for (int i = 0; i < x; i++)
+        {
+            cout << i << " ";
+        }
+        for (int i = x + 1; i < n; i++)
+        {
+            cout << i << " ";
+        }
+        if (x < n)
+        {
+            cout << x;
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
