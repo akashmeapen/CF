2214f76706652dc75354e78f96c8abc3654535e8
Akash Modayil Eapen <akash.modayil@gmail.com>
2025-11-10 18:30:37 +0530

    added 2167A

--- CF/2094A.cpp
+++ CF/2094A.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         string s, s1, s2;
-        cin>>s>>s1>>s2;
-        cout<<s[0]<<s1[0]<<s2[0]<<endl;
-
+        cin >> s >> s1 >> s2;
+        cout << s[0] << s1[0] << s2[0] << endl;
     }
 }
--- CF/2149A.cpp
+++ CF/2149A.cpp
@@ -1,30 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int t;
     cin >> t;
-    while(t--){
+    while (t--)
+    {
         int n;
-        cin>>n;
+        cin >> n;
         vector<int> a(n);
-        for(int i=0;i<n;i++) cin>>a[i];
-        int ops=0,nominus=0;
-        for(int i=0;i<n;i++){
-            if(a[i]==-1){
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        int ops = 0, nominus = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == -1)
+            {
                 nominus++;
             }
-            else{
-                ops+=1-a[i];
+            else
+            {
+                ops += 1 - a[i];
             }
         }
-        if(nominus%2){
-            ops+=2;
+        if (nominus % 2)
+        {
+            ops += 2;
         }
-        cout<<ops<<"\n";
-
-    }   
+        cout << ops << "\n";
+    }
     return 0;
 }
