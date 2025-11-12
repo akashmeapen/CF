4563dac2041d42cbf462e2b17fd7ab18b1483100
Akash Modayil Eapen <akash.modayil@gmail.com>
2025-11-12 08:31:52 +0530

    Mex

---
+++ CF/2160A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, mex = 0;
+        cin >> n;
+        set<int> s;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            s.insert(x);
+        }
+        while (s.find(mex) != s.end())
+            mex++;
+        cout << mex << '\n';
+    }
+}
